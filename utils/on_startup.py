8c6eff6b083e958ab05f479a70a2d696f685fdec
12varam <rahimovumar777@gmail.com>
2026-03-19 20:38:22 +0500

    added utils, it will run the root things

---
+++ utils/on_startup.py
@@ -0,0 +1,7 @@
+from database.tables import create_gallery_table, create_images_table
+
+
+create_gallery_table()
+create_images_table()
+
+# write this command to the terminal: python -m utils.on_startup.py
