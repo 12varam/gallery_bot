e39215f89e2bd1df723e88c9fe078d3ef9f1b3a1
12varam <rahimovumar777@gmail.com>
2026-03-21 00:51:44 +0500

    added function register_user to the route

--- handlers/text.py
+++ handlers/text.py
@@ -1,5 +1,6 @@
 from aiogram import Router, types
 from aiogram.filters.command import Command
+from database.connection import register_user
 
 
 router = Router()
@@ -7,7 +8,12 @@ router = Router()
 
 @router.message(Command("start"))
 async def handle_start(message: types.Message):
-    await message.answer("Welcome to the gallery bot!")
+    register_user(
+        message.from_user.id,
+        message.from_user.username
+    )
+    
+    await message.answer(f"Welcome to the gallery bot {message.from_user.first_name}!")
 
 
 @router.message(Command("info"))
