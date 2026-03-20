4b193b4253024d49d5bf90cf27a2959e5a1b2000
12varam <rahimovumar777@gmail.com>
2026-03-21 00:48:09 +0500

    added 2 functions related to user registering

--- database/connection.py
+++ database/connection.py
@@ -1,5 +1,6 @@
 from psycopg2 import connect
 from config.loader import settings
+from database.connection import connect_to_db
 
 
 def connect_to_db():
@@ -13,3 +14,32 @@ def connect_to_db():
     cursor = connection.cursor()
 
     return connection, cursor
+
+
+def register_user(tg_chat_id, tg_username):
+    connection, cursor = connect_to_db()
+
+    cursor.execute(
+        """
+        INSERT INTO users (tg_chat_id, tg_username) 
+        VALUES (%s, %s) 
+        ON CONFLICT (tg_chat_id) DO NOTHING
+    """,
+        (tg_chat_id, tg_username),
+    )
+
+    connection.commit()
+    connection.close()
+
+
+def get_user_id(tg_chat_id):
+    connection, cursor = connect_to_db()
+
+    cursor.execute("SELECT id FROM users WHERE tg_chat_id = %s", (tg_chat_id,))
+    user_id = cursor.fetchone()
+    connection.commit()
+    connection.close()
+    if user_id:
+        return user_id[0]
+    else:
+        return None
