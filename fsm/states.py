8c91d78445b2f5403a624a4da64560a0c26ffcc5
12varam <rahimovumar777@gmail.com>
2026-03-21 17:11:38 +0500

    added class DeleteGallery

--- fsm/states.py
+++ fsm/states.py
@@ -3,3 +3,7 @@ from aiogram.fsm.state import StatesGroup, State
 
 class CreateGallery(StatesGroup):
     waiting_for_name = State()
+
+
+class DeleteGallery(StatesGroup):
+    waiting_for_name = State()
