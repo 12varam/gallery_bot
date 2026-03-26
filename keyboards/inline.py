54044a74ac1505358c1f4bbeaf940b0b9e147f53
12varam <rahimovumar777@gmail.com>
2026-03-26 21:56:37 +0500

    added buttons to submit/cancel the deletement of the gallery

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -18,19 +18,40 @@ def get_gallery_management_kb(gallery_name):
     builder = InlineKeyboardBuilder()
 
     builder.row(
-        types.InlineKeyboardButton(text="🖼 View Images", callback_data=f"view_{gallery_name}")
+        types.InlineKeyboardButton(
+            text="🖼 View Images", callback_data=f"view_{gallery_name}"
+        )
     )
     builder.row(
         types.InlineKeyboardButton(
             text="➕ Add Photo", callback_data=f"addphoto_{gallery_name}"
         )
-    ) 
-    builder.row(
-        types.InlineKeyboardButton(text="✏️ Rename", callback_data=f"rename_{gallery_name}")
     )
     builder.row(
-        types.InlineKeyboardButton(text="🗑 Delete", callback_data=f"delete_{gallery_name}")
+        types.InlineKeyboardButton(
+            text="✏️ Rename", callback_data=f"rename_{gallery_name}"
+        )
+    )
+    builder.row(
+        types.InlineKeyboardButton(
+            text="🗑 Delete", callback_data=f"delete_{gallery_name}"
+        )
     )
     builder.row(types.InlineKeyboardButton(text="⬅️ Back", callback_data="back_to_list"))
 
+    return builder.as_markup()
+
+
+def get_confirm_delete_kb(gallery_name):
+    builder = InlineKeyboardBuilder()
+
+    builder.row(
+        types.InlineKeyboardButton(
+            text="✅ Yes, delete", callback_data=f"confirm_del_{gallery_name}"
+        ),
+        types.InlineKeyboardButton(
+            text="❌ cancel", callback_data=f"cancel_delete"
+        ),
+    )
+    
     return builder.as_markup()
